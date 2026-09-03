320d7516723b858a1234a1cfc690e1d28a709803
Ojhaji000 <132740226+Ojhaji000@users.noreply.github.com>
2026-09-03 17:50:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace computationalGeometry_cSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
